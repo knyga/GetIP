0c3765b9bed425996c38cd3551fac0059598debd
Oleksandr Knyga <oleksandrknyga@gmail.com>
2014-09-01 15:43:49 +0300

    init

---
+++ GetIP.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GetIP;
+
+class GetIP {
+	public static function get($extended = true) {
+		$ipaddress = '';
+
+	    if ($_SERVER['HTTP_CLIENT_IP']) {
+	        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+	    }
+	    else if($_SERVER['HTTP_X_FORWARDED_FOR']) {
+	        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	    }
+	    else if($_SERVER['HTTP_X_FORWARDED']) {
+	        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+	    }
+	    else if($_SERVER['HTTP_FORWARDED_FOR']) {
+	        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+	    }
+	    else if($_SERVER['HTTP_FORWARDED']) {
+	        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+	    }
+	    else if($_SERVER['REMOTE_ADDR']) {
+	        $ipaddress = $_SERVER['REMOTE_ADDR'];
+	    }
+	    else {
+	        $ipaddress = 'UNKNOWN';
+	    }
+
+	    return $ipaddress;
+	}
+}
